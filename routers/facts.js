709b72f0b8b40dda58500abe9bd1d8dec795c72e
normanvisser <normanvisser88@gmail.com>
2022-06-15 15:14:43 +0200

    Created endpoints: Delete and post facts, delete and post events, delete and post partners

---
+++ routers/facts.js
@@ -0,0 +1,37 @@
+const { Router } = require("express");
+const router = new Router();
+const Fact = require("../models").fact;
+
+router.post("/addNew", async (req, res, next) => {
+  try {
+    const { title, details, partnerId } = req.body;
+
+    const addFact = await Fact.create({
+      title,
+      details,
+      partner_id: partnerId,
+    });
+
+    res.send(addFact);
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
+
+router.delete("/delete", async (req, res, next) => {
+  try {
+    const { factId } = req.body;
+
+    const addFact = await Fact.findByPk(factId);
+
+    addFact.destroy();
+
+    res.send({ message: "Fact deleted", addFact });
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
+
+module.exports = router;
