468472d3e96defc5c1503ff369b961b47f93cb05
git-meaux <maurice.vd.erve@gmail.com>
2022-06-16 12:40:11 +0200

    receiving facts

--- routers/facts.js
+++ routers/facts.js
@@ -4,12 +4,13 @@ const Fact = require("../models").fact;
 
 router.post("/addNew", async (req, res, next) => {
   try {
-    const { title, details, partnerId } = req.body;
+    const { title, details, partnerId, userId } = req.body;
 
     const addFact = await Fact.create({
       title,
       details,
       partner_id: partnerId,
+      user_id: userId,
     });
 
     res.send(addFact);
